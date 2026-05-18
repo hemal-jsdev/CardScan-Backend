ec94e8041e6f56cc57b188599fe4a4e774dfb546
AnshuHemal <anshukatariya4488@gmail.com>
2026-05-18 16:25:33 +0530

    Configure Prisma Service constructor with fallback URL datasource parameter.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,6 +3,15 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL || 'postgresql://fallback:fallback@localhost:5432/fallback',
+        },
+      },
+    });
+  }
   async onModuleInit() {
     try {
       await this.$connect();
